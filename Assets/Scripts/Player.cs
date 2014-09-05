556cf61eb9d500ba5c5a4859b965cb93ed522993
日野森寛也 <nexearth@gmail.com>
2014-09-06 01:00:21 +0900

    第２回まで終了！

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// 移動スピード
+	public float speed = 5;
+	
+	void Update ()
+	{
+		// 右・左
+		float x = Input.GetAxisRaw ("Horizontal");
+		
+		// 上・下
+		float y = Input.GetAxisRaw ("Vertical");
+		
+		// 移動する向きを求める
+		Vector2 direction = new Vector2 (x, y).normalized;
+		
+		// 移動する向きとスピードを代入する
+		rigidbody2D.velocity = direction * speed;
+	}
+}
