02e365d95c95dcf5fcd558a826ea688784880016
日野森寛也 <nexearth@gmail.com>
2014-09-07 12:23:18 +0900

    第３回まで終了！

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public int speed = 10;
+	
+	void Start ()
+	{
+		rigidbody2D.velocity = transform.up.normalized * speed;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,20 @@ public class Player : MonoBehaviour {
 	// 移動スピード
 	public float speed = 5;
 	
+	// PlayerBulletプレハブ
+	public GameObject bullet;
+	
+	// Startメソッドをコルーチンとして呼び出す
+	IEnumerator Start ()
+	{
+		while (true) {
+			// 弾をプレイヤーと同じ位置/角度で作成
+			Instantiate (bullet, transform.position, transform.rotation);
+			// 0.05秒待つ
+			yield return new WaitForSeconds (0.05f);
+		}
+	}
+	
 	void Update ()
 	{
 		// 右・左
