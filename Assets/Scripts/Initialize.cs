829d6bb0f3667005c3434545591993e4fb981ed2
日野森寛也 <nexearth@gmail.com>
2014-09-10 19:50:04 +0900

    localhostに接続して現在時刻を取得する機能実装

---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Initialize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Connect("http://localhost/"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator Connect(string url) {
+		WWW www = new WWW (url);
+		yield return www;
+		if (www.error == null) {
+			Debug.Log("Success : " + www.text);
+		} else {
+			Debug.Log("Failure : " + www.error);
+		}
+	}
+
+}
