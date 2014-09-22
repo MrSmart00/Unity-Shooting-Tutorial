425d8fbbbbfe2a9231c6d0ae47b57aa7401ef44b
日野森寛也 <nexearth@gmail.com>
2014-09-22 18:18:29 +0900

    コード整理

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -1,55 +1,21 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using MiniJSON;
 
 public class Initialize : MonoBehaviour {
 	
-	public Canvas canvas;
 	public GameObject emitter;
 
 	public static string domain = "http://localhost/";
 
 	void Start () {
-		StartCoroutine (ConnectHome(domain));
+		StartCoroutine (ConnectWave(domain + "wave.php"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
-	
-	private IEnumerator ConnectHome(string url) {
-		WWW www = new WWW (url);
-		yield return www;
-		if (www.error == null) {
-			IList json = (IList)Json.Deserialize(www.text);
-			foreach (IDictionary item in json) {
-				Text target = null;
-				foreach (Transform child in canvas.transform){
-					target = child.gameObject.GetComponent<Text>();
-					if(child.name == "Main Title"){
-						target.text = (string)item["title"];
-					} else if (child.name == "Sub Title") {
-						target.text = (string)item["subtitle"];
-					}
-				}
-			}
-			StartCoroutine (ConnectWave(domain + "wave.php"));
-		} else {
-			Text target = null;
-			foreach (Transform child in canvas.transform){
-				target = child.gameObject.GetComponent<Text>();
-				if(child.name == "Main Title"){
-					target.text = "ERROR!!";
-				} else if (child.name == "Sub Title") {
-					target.text = www.error.ToString();
-				}
-				target.color = Color.red;
-			}
-		}
-		yield break;
 	}
 
 	private IEnumerator ConnectWave(string url) {
